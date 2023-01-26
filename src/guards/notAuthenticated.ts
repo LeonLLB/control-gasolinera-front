314569441386bdde1b217e89e194d89d40781c80
Leonard Velásquez <leonard.avp@gmail.com>
2023-01-26 19:09:23 -0400

    Guards corregidos

--- src/guards/notAuthenticated.ts
+++ src/guards/notAuthenticated.ts
@@ -1,11 +1,13 @@
-import { RouteLocationNormalized } from "vue-router";
+import { NavigationGuardNext, RouteLocationNormalized } from "vue-router";
 
-export const notAuthenticated = async (to: RouteLocationNormalized): Promise<string | undefined> => {
+export const notAuthenticated = async (to: RouteLocationNormalized,from:RouteLocationNormalized,next:NavigationGuardNext) => {
+    //TODO: DISPLAY LOADING
     const response = await fetch(import.meta.env.VITE_API_URL+'/api/usuarios/check-auth',{
         credentials:'include'
     })
 
-    if(response.status !== 200){
-        return '/login'
-    }
+    if(response.status === 200){
+        return
+    }    
+    return next()
 }
